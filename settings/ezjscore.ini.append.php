d55a3b1efa063c5118ba6e025f39a4ab853e3c37
Geoff Bentley <geoff@philandteds.com>
2013-03-22 09:59:11 +1300

    updated jquery & jqueryUI details

--- settings/ezjscore.ini.append.php
+++ settings/ezjscore.ini.append.php
@@ -4,5 +4,6 @@
 LoadFromCDN=enabled
 ExternalScripts[jquery]=://code.jquery.com/jquery-1.7.2.min.js
 LocalScripts[jquery]=jquery-1.7.1.min.js
+LocalScripts[jqueryUI]=jquery-ui-1.8.22.custom.min.js
 
 */?>
